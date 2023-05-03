964b935220256ca342737ba7777d5b63e9372d93
Konstantin <iscoulfi@gmail.com>
2023-05-03 14:01:50 +0300

    feat: implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="app"></div>;
-}
+import { Routes, Route } from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+import { Auth, Main, NotFound, Welcome } from 'pages';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="auth" element={<Auth />} />
+          <Route path="main" element={<Main />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
