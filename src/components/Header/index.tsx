b7a04b24188853bb1330d40a9014ae441e1709c7
Konstantin <iscoulfi@gmail.com>
2023-05-04 12:27:10 +0300

    feat: add basic header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar sticky="top" bg="light">
+      <Container>
+        <Navbar.Brand>
+          <img
+            src="/img/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="GraphQL logo"
+          />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
