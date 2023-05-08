a5bfb14a0e9def3ce72b0a89313fcd98765e4c52
Konstantin <iscoulfi@gmail.com>
2023-05-08 10:50:18 +0300

    fix: hide firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,12 +8,12 @@ import {
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDVwc_-LayJEAThYLJoiMw3r4oivN_eXJ4',
-  authDomain: 'graphiql-app-auth-80bdd.firebaseapp.com',
-  projectId: 'graphiql-app-auth-80bdd',
-  storageBucket: 'graphiql-app-auth-80bdd.appspot.com',
-  messagingSenderId: '881819923445',
-  appId: '1:881819923445:web:44dc4f6da8d49bb149a6bc',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
