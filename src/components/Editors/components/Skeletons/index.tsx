bdd42c5e5a6aee5a02e33850e21ba5e78cdd982e
artemmyzrov <a.myzrov@yandex.ru>
2023-05-15 14:54:55 +0400

    feat: add editors skeleton full

--- src/components/Editors/components/Skeletons/index.tsx
+++ src/components/Editors/components/Skeletons/index.tsx
@@ -10,13 +10,15 @@ export const EditorsSkeleton = () => (
     foregroundColor="#ecebeb"
   >
     //Operation Editor Skeleton
-    <rect x="5" y="580" rx="0" ry="0" width="520" height="22" />
+    <rect x="5" y="580" rx="5" ry="5" width="520" height="22" />
+    <rect x="5" y="620" rx="5" ry="5" width="520" height="400" />
     <rect x="5" y="535" rx="5" ry="5" width="80" height="40" />
     <rect x="95" y="535" rx="5" ry="5" width="80" height="40" />
-    <rect x="5" y="15" rx="0" ry="0" width="520" height="22" />
+    <rect x="5" y="15" rx="5" ry="5" width="520" height="22" />
+    <rect x="5" y="55" rx="10" ry="10" width="520" height="460" />
     <rect x="570" y="15" rx="5" ry="5" width="30" height="29" />
     //Response Skeleton
-    <rect x="650" y="14" rx="3" ry="3" width="520" height="22" />
-    <rect x="680" y="56" rx="0" ry="0" width="480" height="850" />
+    <rect x="650" y="14" rx="5" ry="5" width="520" height="22" />
+    <rect x="650" y="56" rx="5" ry="5" width="520" height="850" />
   </ContentLoader>
 );
