1c2f7b9ecf3b4d5a497dfa43d73209c33a695a0e
artemmyzrov <a.myzrov@yandex.ru>
2023-05-20 19:16:40 +0400

    refactor: refactoring Error Boundary

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@ import { store } from 'store';
 import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import { i18n } from 'helpers';
-import ErrorBoundary from 'components/ErrorBoundary';
+import { ErrorBoundary } from 'react-error-boundary';
 import './scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
+    <ErrorBoundary fallback={<div>Loading error, please reload the page or go back..</div>}>
       <BrowserRouter>
         <Provider store={store}>
           <I18nextProvider i18n={i18n}>
