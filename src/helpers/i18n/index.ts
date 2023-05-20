88fc5a35d533ac563e62efa47c22a3b4866ebb38
artemmyzrov <a.myzrov@yandex.ru>
2023-05-20 18:43:24 +0400

    feat: add save language in local storage

--- src/helpers/i18n/index.ts
+++ src/helpers/i18n/index.ts
@@ -2,11 +2,15 @@ import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+const saveLanguageToLocalStorage = (language: string) => {
+  localStorage.setItem('language', language);
+};
+
 i18n
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    lng: 'en',
+    lng: localStorage.getItem('language') || 'en',
     fallbackLng: 'en',
     debug: false,
     backend: {
@@ -16,5 +20,7 @@ i18n
       escapeValue: false,
     },
   });
+
+i18n.on('languageChanged', saveLanguageToLocalStorage);
 
 export { i18n };
