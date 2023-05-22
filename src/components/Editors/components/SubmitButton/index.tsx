aa325dea2edf549a173148304a53577b26088adb
Konstantin <iscoulfi@gmail.com>
2023-05-22 13:59:11 +0300

    feat: change submit button for md size

--- src/components/Editors/components/SubmitButton/index.tsx
+++ src/components/Editors/components/SubmitButton/index.tsx
@@ -1,12 +1,16 @@
+import { useTranslation } from 'react-i18next';
 import Button from 'react-bootstrap/Button';
 import styles from './SubmitButton.module.scss';
 
 import { BsPlayFill, BsStopFill } from 'react-icons/bs';
 
 export const SubmitButton = ({ loading }: { loading: boolean }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.submitButton}>
       <Button variant="primary" disabled={loading} type="submit" size="sm">
+        <span className="d-lg-none fs-6 pe-2">{t('Send')}</span>
         {loading ? <BsStopFill /> : <BsPlayFill />}
       </Button>
     </div>
