1152384251c501c1bf843e76e5abe31ecf20ad7f
Kseniia <chrysostomost0000@gmail.com>
2023-05-22 14:24:36 +0300

    feat: adapt welcome page to various screen sizes

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -26,7 +26,7 @@ export const Welcome = () => {
         <Card.Text className="text-left">{t('Welcome text')}</Card.Text>
         <Card.Text className="text-left">{t('Created')}</Card.Text>
       </Container>
-      <Card className="col-md-4 col-sm-10 col-lg-4 text-justify h-100">
+      <Card className="col-md-4 col-sm-6 col-lg-4 text-justify h-100">
         <Card.Body className="text-center mx-auto">
           <div className={styles.object}>
             <object type="image/svg+xml" data="/img/logo.svg" />
@@ -42,7 +42,7 @@ export const Welcome = () => {
               {t('Go to main page')}
             </Button>
           ) : (
-            <Stack gap={2} className="col-md-8 mx-auto">
+            <Stack gap={2} className="mx-auto col-lg-9">
               <Button onClick={onSignInClick} variant="outline-secondary">
                 {t('Sign in')}
               </Button>
