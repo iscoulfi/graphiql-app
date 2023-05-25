2812c9f79261404bf6b04bb6091e5593aaaa688c
Konstantin <iscoulfi@gmail.com>
2023-05-25 20:39:57 +0300

    feat: add home button to notfound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,16 @@
 import styles from './NotFound.module.scss';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.notFound}>
       <h1>404</h1>
       <h2>Page not found</h2>
+      <Button variant="primary" className="mt-3" onClick={() => navigate('/')}>
+        Home
+      </Button>
     </div>
   );
 };
