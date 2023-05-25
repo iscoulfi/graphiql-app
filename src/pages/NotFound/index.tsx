8ba22ef268a41091229c844d5cc97be9df7ce720
Konstantin <iscoulfi@gmail.com>
2023-05-25 20:47:06 +0300

    feat: add language change on notfound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,15 +1,17 @@
 import styles from './NotFound.module.scss';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export const NotFound = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   return (
     <div className={styles.notFound}>
       <h1>404</h1>
-      <h2>Page not found</h2>
+      <h2>{t('Page not found')}</h2>
       <Button variant="primary" className="mt-3" onClick={() => navigate('/')}>
-        Home
+        {t('Home')}
       </Button>
     </div>
   );
