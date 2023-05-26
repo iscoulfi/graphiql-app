f4e6aaae471acee6c663945754b0c1b9ee1be1c1
Konstantin <iscoulfi@gmail.com>
2023-05-26 18:45:48 +0300

    fix: language enum name

--- src/components/Header/components/LanguageSelector/index.tsx
+++ src/components/Header/components/LanguageSelector/index.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { ButtonGroup, Button } from 'react-bootstrap';
 
-const enum Language {
+const enum Languages {
   RU = 'ru',
   EN = 'en',
 }
@@ -9,24 +9,24 @@ const enum Language {
 export const LanguageSelector = ({ className }: { className: string }) => {
   const { i18n } = useTranslation();
 
-  const changeLanguage = (language: Language) => {
+  const changeLanguage = (language: Languages) => {
     i18n.changeLanguage(language);
   };
 
   return (
     <ButtonGroup size="sm" className="ms-2">
       <Button
-        active={i18n.language === Language.EN}
+        active={i18n.language === Languages.EN}
         variant="outline-secondary"
-        onClick={() => changeLanguage(Language.EN)}
+        onClick={() => changeLanguage(Languages.EN)}
         className={className}
       >
         EN
       </Button>
       <Button
-        active={i18n.language === Language.RU}
+        active={i18n.language === Languages.RU}
         variant="outline-secondary"
-        onClick={() => changeLanguage(Language.RU)}
+        onClick={() => changeLanguage(Languages.RU)}
         className={className}
       >
         RU
